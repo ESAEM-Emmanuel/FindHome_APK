0b4534daa047c558b583670e1a69322207222dc6
EmmanuelBFC <eaoudou@bfcgroupsa.com>
2025-11-04 16:29:43 +0100

    network autoriisation.

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -2,5 +2,5 @@
 class ApiConstants {
   // static const String baseUrl = 'http://127.0.0.1:8000'; 
   // static const String baseUrl = 'http://172.19.120.38:8000'; 
-  static const String baseUrl = 'http://172.20.10.9:8000'; 
+  static const String baseUrl = 'https://findhome-api.onrender.com'; 
 }
